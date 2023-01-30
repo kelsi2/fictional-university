95bd147e2570271d73393530b85bfdd40d8eb9b9
Kelsi Proulx <kelsiproulx1919@gmail.com>
2023-01-29 19:10:07 -0800

    Add mu-plugins folder with custom Event post type

---
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,19 @@
+<?php
+function university_post_types()
+{
+	register_post_type("event", [
+		"public" => true,
+		"show_in_rest" => true,
+		"labels" => [
+			"name" => "Events",
+			"add_new_item" => "Add New Event",
+			"edit_item" => "Edit Event",
+			"all_items" => "All Events",
+			"singular_name" => "Event",
+		],
+		"menu_icon" => "dashicons-calendar",
+	]);
+}
+
+add_action("init", "university_post_types");
+?>
