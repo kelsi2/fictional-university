301f570e6683aa2022bcbcaebcfe0484a6691a8d
Kelsi Proulx <kelsiproulx1919@gmail.com>
2023-02-01 15:27:44 -0800

    Create Program post type and link events

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -7,17 +7,34 @@ get_header(); ?>
       	"/images/ocean.jpg"
       ); ?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
+        <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-          <p>See what is going on in our world.</p>
+          <p>A recap of our past events.</p>
         </div>
       </div>
     </div>
 
     <div class="container container--narrow page-section">
       <?php
-      while (have_posts()) {
-      	the_post(); ?>
+      $today = date("Ymd");
+      $pastEvents = new WP_Query([
+      	"paged" => get_query_var("paged", 1),
+      	"post_type" => "event",
+      	"meta_key" => "event_date",
+      	"orderby" => "meta_value_num",
+      	"order" => "ASC",
+      	"meta_query" => [
+      		[
+      			"key" => "event_date",
+      			"compare" => "<",
+      			"value" => $today,
+      			"type" => "numeric",
+      		],
+      	],
+      ]);
+
+      while ($pastEvents->have_posts()) {
+      	$pastEvents->the_post(); ?>
 
         <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
@@ -42,13 +59,10 @@ get_header(); ?>
       <?php
       }
 
-      echo paginate_links();
+      echo paginate_links([
+      	"total" => $pastEvents->max_num_pages,
+      ]);
       ?>
-
-      <hr class="section-break">
-      <p>Looking for a recap of past events? <a href="<?php echo site_url(
-      	"/past-events"
-      ); ?>">Check out our past events archive</a>.</p>
     </div>
 
 <?php get_footer();
